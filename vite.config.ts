18c6f1c5bf830655252964e3ac512d3032fa1038
Emin <90508482+eminbaycan@users.noreply.github.com>
2026-08-07 11:42:55 +0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,6 +0,0 @@
-{
-  "name": "Emin Baycan Portfolio",
-  "description": "Minimalist CV and Portfolio for Emin Baycan with an easter egg.",
-  "requestFramePermissions": [],
-  "majorCapabilities": ["MAJOR_CAPABILITY_SERVER_SIDE_GEMINI_API"]
-}
